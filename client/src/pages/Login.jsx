f01b97940678db14d2e2fb18f18f7487dfcf29d3
boylic <malicksixnine@gmail.com>
2023-07-05 09:38:06 +0300

    commit

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -26,7 +26,7 @@ function Copyright(props) {
     >
       {"Copyright © "}
       <Link color="inherit" to="/">
-        RCOS
+        RCOBS
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
